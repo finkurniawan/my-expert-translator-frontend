3009d9a60d338482a6faeb32d0d18c787b980f4d
Alfin Kurniawan Aryanto <at.alfinkurniawan@gmail.com>
2024-07-19 13:43:59 +0700

    feat: implement API translate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,28 @@
-import Image from "next/image";
+'use client';
 import Card from "@/components/Card";
+import {SetStateAction, useEffect, useState} from "react";
+import axios from "axios";
 
 export default function Home() {
-  return (
-   <main>
-<div className={"flex flex-row items-center justify-center px-16 bg-gray-100 gap-1"}>
-    <Card placeholder={"your text"}/>
-    <Card placeholder={"output text"}/>
-</div>
-   </main>
-  );
+    const [text, setText] = useState("");
+    const [output, setOutput] = useState("");
+
+    const handleActionCard = (text: String): void => {
+        // @ts-ignore
+        setText(text);
+    }
+
+    useEffect(() => {
+        axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/translate?text=${text}&targetLanguage=${"en"}`).then(({data}) => setOutput(data.translatedText)).catch((e) => console.log(e))
+    }, [text])
+
+    return (
+        <main>
+            <div className={"flex flex-row items-center justify-center px-16 bg-gray-100 gap-1"}>
+                <Card placeholder={"your text"} handleAction={handleActionCard} key={1} type="input"/>
+                <Card placeholder={"output text"} handleAction={(_) => {
+                }} output={output} key={2} type="output"/>
+            </div>
+        </main>
+    );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,7 @@
-export default function Card({placeholder}: {placeholder: string}) {
-    return <textarea className={"bg-white w-full h-56 shadow-lg rounded-md"} placeholder={placeholder}></textarea>
+'use client';
+import debounce from "debounce";
+
+export default function Card({placeholder,handleAction,output,type}: {placeholder: string,handleAction?: (text: String) => void,output?: string,type: string}) {
+    // @ts-ignore
+    return <textarea className={"bg-white w-full h-56 shadow-lg rounded-md"} placeholder={placeholder} onChange={debounce((v) => handleAction(v.target.value),300)} value={output}></textarea>
 }
