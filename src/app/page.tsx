efb8352ef0df24bbb68ca5900b5b0eceb1483017
Alfin Kurniawan Aryanto <at.alfinkurniawan@gmail.com>
2024-07-19 14:29:44 +0700

    add support multiple target language translate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,38 @@
 import Card from "@/components/Card";
 import {SetStateAction, useEffect, useState} from "react";
 import axios from "axios";
+import {languages} from "@/data/languages";
 
 export default function Home() {
     const [text, setText] = useState("");
     const [output, setOutput] = useState("");
-
+    const [languageCode,setLanguageCode] = useState("");
     const handleActionCard = (text: String): void => {
         // @ts-ignore
         setText(text);
     }
 
+    const handleChangeLanguage = (value: SetStateAction<string>): void => {
+        setLanguageCode(value)
+    }
+
     useEffect(() => {
-        axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/translate?text=${text}&targetLanguage=${"en"}`).then(({data}) => setOutput(data.translatedText)).catch((e) => console.log(e))
+        axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/translate?text=${text}&targetLanguage=${languageCode}`).then(({data}) => setOutput(data.translatedText)).catch((e) => console.log(e))
     }, [text])
 
     return (
         <main>
-            <div className={"flex flex-row items-center justify-center px-16 bg-gray-100 gap-1"}>
-                <Card placeholder={"your text"} handleAction={handleActionCard} key={1} type="input"/>
-                <Card placeholder={"output text"} handleAction={(_) => {
-                }} output={output} key={2} type="output"/>
+            <div className={"flex flex-row items-end justify-center px-16 bg-gray-100 gap-1"}>
+               <div className={"w-full"}>
+                   <Card placeholder={"your text"} handleAction={handleActionCard} key={1} type="input"/>
+               </div>
+              <div className={"w-full"}>
+                  <select onChange={(v) => handleChangeLanguage(v.target.value)}>
+                      {languages.map((v,i) => <option key={i} value={v.code}>{v.name}</option>)}
+                  </select>
+                  <Card placeholder={"output text"} handleAction={(_) => {
+                  }} output={output} key={2} type="output"/>
+              </div>
             </div>
         </main>
     );
